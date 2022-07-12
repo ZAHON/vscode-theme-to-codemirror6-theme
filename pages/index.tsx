4d314dc87a342aac6b6b2453f3baceaaa6ecb421
Konrad Trusiuk <konrad.zahon@gmail.com>
2022-07-12 17:58:11 +0200

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>VCcode theme to CodeMirror6 theme</title>
+        <meta name="description" content="VCcode theme to CodeMirror6 theme" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Works!</h1>
+    </>
+  );
+};
+
+export default Home;
